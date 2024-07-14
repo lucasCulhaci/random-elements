cd8ecb0a43918356e531d029090cac08d5986390
Lucas Culhaci <culhacii.lucas@gmail.com>
2024-07-15 00:13:04 +0200

    Opgelost

--- js/find-the-green-block.js
+++ js/find-the-green-block.js
@@ -1,31 +1,36 @@
 'use strict';
 
+// Start variabelen
 let container = document.querySelector('.grid-container');
-let item = document.querySelectorAll('.grid-item');
-
 let randomNumber = 0;
+let listOfItems = [];
 
+let amountOfItems = Number(prompt('Hoeveel items wenst u?'));
+
+// Functie voor het initialiseren
 const init = function () {
-  randomNumber = Math.trunc(Math.random() * item.length) + 1;
+  randomNumber = Math.trunc(Math.random() * amountOfItems) + 1;
 };
 
-let amountOfItems = 9;
-// Adding Grid Items
-
-// TODO: Voeg de grid-items toe gebruik makend van een loop
+// Items Toevoegen
 const addGridItem = function () {
   for (let item = 0; item < amountOfItems; item++) {
-    container.insertAdjacentElement(
-      'afterend',
-      `<div class="grid-item" id="${item}"`
+    container.insertAdjacentHTML(
+      'beforeend',
+
+      `<div class="grid-item" id="${item + 1}"></div>`
     );
+
+    listOfItems.push(item);
   }
 };
 
+// Initialiseren
 init();
+addGridItem();
+let item = document.querySelectorAll('.grid-item');
 
-console.log(randomNumber);
-
+// Item Selecteren en Beoordelen
 container.addEventListener('click', function (e) {
   let clickedItem = e.target.id;
   if (clickedItem == randomNumber) {
